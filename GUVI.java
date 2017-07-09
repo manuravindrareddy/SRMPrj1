cab6aeecfc6f648d16fa7f70d4fee1d5c8e271d7
manuravindrareddy <ravininjeti@gmail.com>
2017-07-09 11:49:04 +0530

    Add files via upload

---
+++ GUVI.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args)
+	{
+	 String name = 'Sridevi';
+     String city = 'Chennai';
+     byte age = 32;
+     float height = 5.3f;
+     boolean married = true;
+     char sex = 'F';
+     
+     
+	}
+}
